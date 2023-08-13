c12245a7d8c9202a281970786622e643a9238043
jkantiwal <jaswinder.kantiwal@hallowelt.com>
2023-08-13 11:54:25 +0200

    done by ankit

---
+++ files/data_types.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    string phrase = "Old is gold"
+    cout << phrase.length();
+    return 0;
+}
---
+++ files/variable.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    
+    string charName = "Johan";
+    string newName = "chutiya";
+    int charAge;
+    24;
+    cout << "His name is " << charName << endl;
+    cout << "He is " << charAge << endl;llllll
+    cout << "He is " << newName << ", that's who ankit is" << endl;
+
+    return 0;
+}
